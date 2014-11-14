b5dcad14853f316bc7da0ec1535fc1f6b9e26040
Wouter Beek <me@wouterbeek.com>
2014-11-14 05:59:12 +0100

    1. DEPLOYMENT: Use modern load/debug startup. 2. Upped submodules.

--- debug.pl
+++ debug.pl
@@ -1,9 +1,7 @@
 % Loads debug tools for the Model-Theory library.
 
-:- use_module(library(ansi_term)).
-
-% Avoid errors when using gtrace/0 in threads.
-:- initialization(guitracer).
-
 :- [load].
 
+% Load all files.
+:- use_module(debug_project).
+:- debug_all_files.
