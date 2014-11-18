ae8160797375b2cccb154632ebc0f4d8b4fa8454
Wouter Beek <me@wouterbeek.com>
2014-11-18 21:39:35 +0100

    FIXED: Project name must be identical to Git project name.

--- load.pl
+++ load.pl
@@ -2,7 +2,7 @@
 
 :- dynamic(user:project/3).
 :- multifile(user:project/3).
-   user:project('Model-Theory', 'Prolog implementation of Model Theory', mt).
+   user:project('ModelTheory', 'Prolog implementation of Model Theory', mt).
 
 :- use_module(load_project).
 :- load_project([
